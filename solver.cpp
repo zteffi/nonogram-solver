e119a92dc48fa1d75b2c5d169cf0fbd8b2b08b6d
Martin Stefanak <zteffi@gmail.com>
2026-02-17 17:51:09 +0100

    bruteforce stars and bars method

--- solver.cpp
+++ solver.cpp
@@ -1,6 +1,10 @@
 #include "solver.hpp"
 
 #include <iostream>
+#include <numeric>
+#include <bitset>
+#include <limits>
+#include <bit>
 
 namespace {
 
@@ -12,143 +16,142 @@ struct overloaded : Ts... { using Ts::operator()...; };
 template<class... Ts>
 overloaded(Ts...) -> overloaded<Ts...>;
 
+size_t binomial_coefficient(size_t n, size_t r){
+
+    size_t sum = 1;
+    for (size_t i = 1; i <= r; i++) {
+        sum = sum * (n - r + i) / i;
+    }
+    return sum;
 }
 
-using LineContext = std::vector<std::vector<std::variant<Solver::WhiteSpace, Solver::BlackSpace>>>;
+void print(const std::vector<size_t>& img)
+{
+
+	for (auto row: img)
+	{
+
+		std::cout << std::bitset<5>(row) << '\n';
+	}
+	std::cout << img.size()<< "------------\n";
+}
+
+bool validate(const std::vector<size_t>& candidate,
+	const std::vector<std::vector<uchar>>& cols) {
+	for (uchar i = 0; i < cols.size(); i++) {
+		size_t col = 0;
+		for (auto& r : candidate) {
+			col <<= 1;
+			col += ((1<<i)&r) >> i;
+		}
+		auto it = cols[i].rbegin();
+		uchar oneCount = 0;
+		for (;it != cols[i].rend(); it++) {
+			while (!(col % 2)) {
+				col >>= 1;
+				if (!col) {
+					return false;
+				}
+			}
+			while (col % 2) {
+				oneCount++;
+				col >>= 1;
+			}
+			if (oneCount != *it) {
+				return false;
+			}
+			oneCount = 0;
+		}
+	}
+	return true;
+}
+
+bool next_combination(std::vector<uchar>& comb, int n) {
+    int k = comb.size();
+    for (int i = k - 1; i >= 0; --i) {
+        if (comb[i] < n - k) {
+            ++comb[i];
+            for (int j = i + 1; j < k; ++j) {
+                comb[j] = comb[j - 1] + 1;
+            }
+            return true;
+        }
+    }
+    return false;
+}
+
+}
 
 
-Solver::Solver(size_t width, size_t height, std::vector<std::vector<int>> cols, std::vector<std::vector<int>> rows)
+
+Solver::Solver(size_t width, size_t height, std::vector<std::vector<uchar>> cols, std::vector<std::vector<uchar>> rows)
 	: _width(width)
 	, _height(height)
+	, _cols(cols)
+	, _rows(rows)
 {
-	std::vector<Pixel> row{ width, {COLOR::UNDEFINED, nullptr, nullptr } };
-	_state.resize(height, row);
-	_rows = InitializeLines(width, rows);
-	_cols = InitializeLines(height, cols);
+	PrepareStates();
+
 }
 
-LineContext Solver::InitializeLines(size_t dimension, std::vector<std::vector<int>>& blackSpaceSizes) {
-	LineContext lines;
-	lines.resize(blackSpaceSizes.size(), {WhiteSpace{0, 0}});
-	for (size_t i = 0; i < blackSpaceSizes.size(); i++) {
-		size_t sz_counter = 0;
-		for (size_t j = 0; j < blackSpaceSizes[i].size(); j++) {
-			size_t blackElemSize = blackSpaceSizes[i][j];
-			lines[i].push_back(BlackSpace{sz_counter, blackElemSize});
-			sz_counter += blackElemSize;
-			lines[i].push_back(WhiteSpace{sz_counter});
-			sz_counter += 1;
-		}
-		sz_counter = dimension;
-		auto& last_white = std::get<WhiteSpace>(lines[i].back());
-		last_white.size_current = 0;
-		for (auto it = lines[i].rbegin(); it != lines[i].rend(); it++)
+void Solver::PrepareStates()
+{
+	_combinations_and_stars_pairs.reserve(_rows.size());
+	for (const auto& row : _rows)
+	{
+		// stars and bars
+		auto bars = row.size();
+		auto stars = _width -  std::accumulate(row.begin(), row.end(), bars-1);
+		auto combinations = binomial_coefficient(stars + bars, bars);
+		_combinations_and_stars_pairs.push_back({combinations, stars});
+
+	}
+ }
+
+
+void Solver::Solve()
+{
+	//bruteforce
+	size_t val = std::accumulate(_combinations_and_stars_pairs.begin(), _combinations_and_stars_pairs.end(), 1,
+		[](size_t acc, auto& b){ return acc* std::get<0>(b); }
+	);
+	std::vector<std::vector<uchar>> vec;
+	for (auto row: _rows)
+	{
+		vec.push_back(std::vector<uchar>(row.size() + 1));
+		std::iota(vec.back().begin(), vec.back().end(), 0);
+	}
+
+	std::cout << "Trying " << val << " times\n";
+	std::vector<size_t> img(_rows.size(), 0);
+	for (size_t i = 0; i < val; i++) {
+
+		auto index = 0;
+		for (index = 0; index < vec.size(); index++)
 		{
-			std::visit(overloaded {
-
-				[&sz_counter](BlackSpace& b) {
-					b.max_end = sz_counter;
-					sz_counter -= b.size;
-				},
-				[&sz_counter](WhiteSpace& w) {
-					w.max_end = sz_counter;
-					sz_counter -= w.size_current;
+			bool next = next_combination(vec[index], static_cast<int>(std::get<0>(_combinations_and_stars_pairs[index]))) && i > 0;
+			if (!next) {
+				std::iota(vec.back().begin(), vec.back().end(), 0);
+			}
+			img[index] = 0;
+			for (auto j = _rows[index].size() - 1; j != std::numeric_limits<size_t>::max(); j--) {
+				img[index] |= (1<<_rows[index][j]) - 1;
+				size_t ws = 0;
+				if (j) {
+					ws = vec[index][j] - vec[index][j-1] +  _rows[index][j];
+				} else {
+					ws = vec[index][j];
 				}
-			}, *it);
+				img[index] <<= ws;
+			}
 		}
-	}
-	return lines;
-}
-
-void Solver::Solve() {
-	int index = 0;
-	for (auto& row : _rows) {
-		for (auto& e : row) {
-			std::visit(overloaded {
-				[&](BlackSpace& b) {
-					if (int sz = b.size_current(); sz > 0) {
-						auto margin = b.size - sz;
-						for (int x = b.min_start+margin; x < b.max_end-margin; x++) {
-							_state[index][x].color = COLOR::BLACK;
-							_state[index][x].rowElem = &b;
-						}
-					}
-				},
-				[&](WhiteSpace& w) {
-					if (w.size_current == w.max_end - w.min_start) {
-						for (int x = w.min_start; x < w.max_end; x++) {
-							_state[index][x].color = COLOR::WHITE;
-							_state[index][x].rowElem = &w;
-						}
-
-					}
-				}
-			}, e);
+		std::cout << i << ": "<< std::endl;
+		if (validate(img, _cols)) {
+			std::cout << "Solution!\n";
+			print(img);
+			return;
 		}
-		++ index;
-	}
-	index = 0;
-	for (auto& col : _cols) {
-		for (auto& e : col) {
-			std::visit(overloaded {
-				[&](BlackSpace& b) {
-					if (int sz = b.size_current(); sz > 0) {
-						auto margin = b.size - sz;
-						for (int y = b.min_start+margin; y < b.max_end-margin; y++) {
-							_state[y][index].color = COLOR::BLACK;
-							_state[y][index].rowElem = &b;
-						}
-					}
-				},
-				[&](WhiteSpace& w) {
-					if (w.size_current == w.max_end - w.min_start) {
-						for (int y = w.min_start; y < w.max_end; y++) {
-							_state[y][index].color = COLOR::WHITE;
-							_state[y][index].rowElem = &w;
-						}
-						std::cout << index << " White(" <<w.size_current<< "): "  << w.min_start << ">> " << w.max_end  << std::endl;
-					}
-				}
-			}, e);
 
-		}
-		++ index;
 	}
 }
 
-void Solver::Print() {
-	for (auto& rr : _cols) {
-		for (auto e : rr) {
-				std::visit(overloaded {
-
-				[](BlackSpace& b) {
-					std::cout << "B (" << b.size_current() << "/" << b.size << ") " << b.min_start << " :" << b.max_end << " | ";
-				},
-				[](WhiteSpace& w) {
-				}
-			}, e);
-		}
-		std::cout << std::endl;
-	}
-
-	int ridx = 0;
-	for (auto& row : _state) {
-		int idx = 0;
-		for (auto& elem : row) {
-			switch (elem.color) {
-			case COLOR::UNDEFINED:
-					std::cout << " /";
-					break;
-				 case COLOR::WHITE:
-					std::cout << "  ";
-				break;
-				 case COLOR::BLACK:
-					 std::cout << " B";
-					break;
-
-			};
-			std::cout << ((++idx)%5 ? " ": "|");
-		}
-		std::cout << std::endl << ((++ridx%5)?"":"--------------|--------------|--------------|--------------|\n");
-	}
-}
--- solver.hpp
+++ solver.hpp
@@ -1,50 +1,28 @@
 #pragma once
-#include <variant>
 #include <vector>
 
+using uchar = unsigned char;
+
 class Solver {
+
 public:
-Solver(size_t width, size_t height, std::vector<std::vector<int>> cols, std::vector<std::vector<int>> rows);
-struct BlackSpace {
-	size_t min_start;
-	size_t max_end;
-	size_t size;
-	int size_current() const {
-		return static_cast<int>(min_start) + 2 * size - max_end;
-	}
-	BlackSpace(size_t min, size_t sz) : min_start(min),  max_end(0), size(sz) {}
-};
-struct WhiteSpace {
-	size_t size_current;
-	size_t min_start;
-	size_t max_end;
-	WhiteSpace(size_t min, size_t sz = 1) : size_current(sz),  min_start(min), max_end(0) {}
-
-};
+	Solver(size_t width, size_t height, std::vector<std::vector<uchar>> cols, std::vector<std::vector<uchar>> rows);
 
 
-enum class COLOR {
-	UNDEFINED,
-	WHITE,
-	BLACK
-};
 
-struct Pixel {
-	enum COLOR color;
-	std::variant<nullptr_t, WhiteSpace*, BlackSpace*> rowElem;
-	std::variant<nullptr_t, WhiteSpace*, BlackSpace*> colElem;
-};
+	enum class COLOR {
+		UNDEFINED,
+		WHITE,
+		BLACK
+	};
 
-void Solve();
-void Print();
-
-using LineContext = std::vector<std::vector<std::variant<WhiteSpace, BlackSpace>>>;
-
+	void Solve();
 private :
-	static LineContext InitializeLines(size_t dimension, std::vector<std::vector<int>>& blackSpaceSizes);
+	void PrepareStates();
+
+	std::vector<std::tuple<size_t, uchar>> _combinations_and_stars_pairs;
+	std::vector<std::vector<uchar>> _cols;
+	std::vector<std::vector<uchar>> _rows;
 	size_t _width;
 	size_t _height;
-	std::vector<std::vector<Pixel>> _state;
-	std::vector<std::vector<std::variant<WhiteSpace, BlackSpace>>> _rows;
-	std::vector<std::vector<std::variant<WhiteSpace, BlackSpace>>> _cols;
 };
